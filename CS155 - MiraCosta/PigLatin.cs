88ffadf4931a19c0b02a86cc717a3a64fe72a32b
parke <parke@PARKERS_ASUS>
2024-09-01 23:49:50 -0700

    Add project files.

---
+++ CS155 - MiraCosta/PigLatin.cs
@@ -0,0 +1,38 @@
+﻿/// Homework No. 2
+/// File Name: PigLatin.cs
+/// @author: Parker Verran
+/// Date:  September 1, 2024
+///
+/// Problem Statement: Develop a program to turn a user's full name into pig latin and print it to the console
+/// 
+/// 
+/// Overall Plan:
+/// 1) Create a new first name variable that equals the name minus the first letter, plus the first letter at the end, plus "ay"
+/// 2) Repeat the above process for the last name
+/// 3) Create a new string concatenating the first and last names
+/// 4) Print the new pig latin name to the console
+
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace std;
+
+class PigLatin
+{
+    static void Main(string[] args)
+    {
+        //declare the name variables
+        string first = "parker";
+        string last = "verran";
+
+        //convert the first name into pig latin
+        string first_pig = (Char.ToUpper(first[1]) + first.Substring(2) + first[0] + "ay");
+        //convert the last name into pig latin
+        string last_pig = (Char.ToUpper(last[1]) + last.Substring(2) + last[0] + "ay");
+
+        //concatenate the two strings into a finalized name
+        string pig_latin_name = first_pig + " " + last_pig;
+        //print the name to the console
+        Console.WriteLine(pig_latin_name);
+    }
+}
---
+++ Homework2_Part2/TempConversions.cs
@@ -0,0 +1,34 @@
+﻿/// Homework No. 2
+/// File Name: TempConversions.cs
+/// @author: Parker Verran
+/// Date:  September 1, 2024
+///
+/// Problem Statement: Develop a program to convert degrees Fahrenheit to degrees Celsius
+/// 
+/// 
+/// Overall Plan:
+/// 1) Retrieve user input for the temperature in fahrenheit
+/// 2) Convert this temperature to celsius using the formula C = 5(F - 32)/9
+/// 3) Round this result to one decimal place
+/// 4) Print the result to the console
+
+namespace std;
+
+using System;
+
+class TempCoversions
+{
+    static void Main(string[] args)
+    {
+        //Get user input
+        Console.WriteLine("Input a Whole Number Temperature in Degrees Fahrenheit: ");
+        string input = Console.ReadLine();
+        int degrees_F = Int32.Parse(input);
+
+        //Convert fahrenheit to celsius
+        double degrees_C = Math.Round((5 * (degrees_F - 32) / 9.0), 1);
+
+        //Print the result to the console
+        Console.WriteLine("\n{0} degrees Fahrenheit = {1} degrees Celsius", degrees_F, degrees_C);
+    }
+}
