efd408a53a58fa1ce829e278de54862d691b12fc
Parker Verran <parkerverran@gmail.com>
2024-09-12 23:43:16 -0700

    Completed Homework 3, Parts 1 and 2.

---
+++ Homework3_Part1/PigLatinConverter.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Homework3_Part1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            string full_name = NameBox.Text;
+            string pig_name = ConvertName(full_name);
+
+            OutputBlock.Text = "Your name in pig latin is " + pig_name;
+        }
+
+        public string ConvertName(string name)
+        {
+            string[] names = name.Split(' ');
+            string first = names[0];
+            string last = names[1];
+
+            //convert the first name into pig latin
+            string first_pig = (Char.ToUpper(first[1]) + first.Substring(2) + Char.ToLower(first[0]) + "ay");
+            //convert the last name into pig latin
+            string last_pig = (Char.ToUpper(last[1]) + last.Substring(2) + Char.ToLower(last[0]) + "ay");
+
+            //concatenate the two strings into a finalized name
+            string pig_latin_name = first_pig + " " + last_pig;
+            return pig_latin_name;
+        }
+    }
+}
---
+++ Homework3_Part2/ChangeCalculator.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Homework3_Part2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            string price = OptionsBox.Text;
+            double price_d = Convert.ToDouble(price.Substring(1));
+            Console.WriteLine(price_d);
+
+            int[] change = CalculateChange(price_d);
+            Console.WriteLine(change);
+            CalculatedChangeBlock.Text = $"You bought an item for {price} and gave me $1.00, so your total change is ${Math.Round(1.0 - price_d, 2)}, which is {change[0]} quarters, {change[1]} dimes, and {change[2]} nickels.";
+        }
+        public int[] CalculateChange(double price)
+        {
+            var change = new int[3];
+            double change_owed = 1 - price;
+            int num_quarters = 0;
+            int num_dimes = 0;
+            int num_nickels = 0;
+
+           while ((change_owed - 0.25) >= 0)
+            {
+                num_quarters++;
+                change_owed -= 0.25;
+            }
+            while ((change_owed - 0.1) >= 0)
+            {
+                num_dimes++;
+                change_owed -= 0.1;
+            }
+            while ((change_owed - 0.05) >= 0)
+            {
+                num_nickels++;
+                change_owed -= 0.05;
+            }
+
+            change[0] = num_quarters;
+            change[1] = num_dimes;
+            change[2] = num_nickels;
+            return change;
+        }
+    }
+}
